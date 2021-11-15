c47df3543fed48169989c17114109305881a7605
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2021-11-15 14:52:55 -0500

    uploaded to github for access

---
+++ Assets/ColliderExits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderExits : MonoBehaviour
+{
+    public GameObject colliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+}
